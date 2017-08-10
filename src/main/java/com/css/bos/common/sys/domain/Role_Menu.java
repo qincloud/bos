98618de62de42cab7b9289edcf824d8eef1b369a
laozq <996298643@qq.com>
2017-08-11 00:02:11 +0800

    commit

---
+++ src/main/java/com/css/bos/common/sys/domain/Role_Menu.java
@@ -0,0 +1,25 @@
+package com.css.bos.common.sys.domain;
+
+public class Role_Menu {
+
+	private String roleid;
+
+	private String menuid;
+
+	public String getRoleid() {
+		return roleid;
+	}
+
+	public void setRoleid(String roleid) {
+		this.roleid = roleid;
+	}
+
+	public String getMenuid() {
+		return menuid;
+	}
+
+	public void setMenuid(String menuid) {
+		this.menuid = menuid;
+	}
+
+}
---
+++ src/main/java/com/css/bos/common/sys/domain/User_Role.java
@@ -0,0 +1,24 @@
+package com.css.bos.common.sys.domain;
+
+public class User_Role {
+
+	private String userid;
+	private String roleid;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getRoleid() {
+		return roleid;
+	}
+
+	public void setRoleid(String roleid) {
+		this.roleid = roleid;
+	}
+
+}
